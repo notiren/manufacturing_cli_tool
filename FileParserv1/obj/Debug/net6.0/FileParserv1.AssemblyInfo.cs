a9d476854d7dc33b6ba771e186c4bc4750a140e2
notiren <pacarizi.neriton@gmail.com>
2025-07-14 13:16:03 +0200

    updated README.MD

--- FileParserv1/obj/Debug/net6.0/FileParserv1.AssemblyInfo.cs
+++ FileParserv1/obj/Debug/net6.0/FileParserv1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileParserv1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b069ac5172ce068bd4e44027e130e03e11554c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileParserv1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileParserv1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ python cli_tool.py
 Choose script from the menu  
 Drag/drop or paste the file path  
 Enjoy !  
+  
+
+Scripts Menu 
+Script 1 - Download Images from URL (used for downloading the testing images via an Excel sheet containing URLs)
+Script 2 - Analyze JSON/ZIP (analyze data of PoE EE Load or Adapter Edac from JSON files, accepts single or multiple files inside a ZIP archive)
+Script 3 - Convert CSV to Excel  
+Script 4 - Split CSV Tests  
+Script 5 - File Parser  
