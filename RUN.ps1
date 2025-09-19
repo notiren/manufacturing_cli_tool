8ac8b12ed90bf66efa07728ae4e7887a1c3d479b
notiren <pacarizi.neriton@gmail.com>
2025-09-19 12:24:07 +0200

    refactoring

--- RUN.ps1
+++ RUN.ps1
@@ -1,1 +1,9 @@
-powershell "$PSScriptRoot\cli_tool.py"
+# RUN.ps1
+# Full path to Python executable (if in PATH, just "python" works)
+$pythonExe = "python"
+
+# Full path to the Python script
+$scriptPath = Join-Path $PSScriptRoot "cli_tool.py"
+
+# Run Python with the script
+& $pythonExe "$scriptPath"
